7e12e5663403f01ba16262d6067351116f09c93c
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-12-23 11:38:17 -0300

    added projects

--- src/services/photos.js
+++ src/services/photos.js
@@ -1,24 +1,24 @@
 export const getPhotos = async (photosPerPage, callback) => {
-  const apiKey = import.meta.env.VITE_KEY_API_PEXELS;
-  const baseUrl = "https://api.pexels.com/v1/curated";
+    const apiKey = import.meta.env.VITE_KEY_API_PEXELS;
+    const baseUrl = "https://api.pexels.com/v1/curated";
 
-  let response = [];
-  try {
+    let response = [];
+    try {
     response = await fetch(`${baseUrl}?page=1&per_page=${photosPerPage}`, {
         method: "get",
         headers: new Headers({
         Authorization: apiKey,
-      }),
+        }),
     })
-      .then((response) => response.json())
-      .then((data) => data.photos);
+        .then((response) => response.json())
+        .then((data) => data.photos);
 
     return response;
-  } catch (error) {
+    } catch (error) {
     console.log(error);
-  } finally {
+    } finally {
     callback();
-  }
+    }
 
-  return response;
+    return response;
 };
